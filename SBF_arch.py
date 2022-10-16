2fef20df6212b4b216f4a9751001a81d2b66675c
psubodiosa <70536848+psubodiosa@users.noreply.github.com>
2022-10-16 10:13:15 -0400

    Add files via upload

---
+++ SBF_arch.py
@@ -0,0 +1,234 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch import Tensor
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange, Reduce
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels: int = 3, patch_size: int = 8, emb_size: int = 768, img_size: int = 224):
+        self.patch_size = patch_size
+        super().__init__()
+        self.projection = nn.Sequential(
+            # using a conv layer instead of a linear one -> performance gains
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b e (h) (w) -> b (h w) e'),
+        )
+        self.cls_token = nn.Parameter(torch.randn(1, 1, emb_size))
+        self.positions = nn.Parameter(torch.randn((img_size // patch_size) ** 2 + 1, emb_size))
+
+    def forward(self, x: Tensor) -> Tensor:
+        b, _, _, _ = x.shape
+        x = self.projection(x)
+        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
+        # prepend the cls token to the input
+        x = torch.cat([cls_tokens, x], dim=1)
+        # add position embedding
+        x += self.positions
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_size: int = 512, num_heads: int = 8, dropout: float = 0):
+        # Theoretically, the larger the embedding size, the more information can be learnt
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.keys = nn.Linear(emb_size, emb_size)
+        self.queries = nn.Linear(emb_size, emb_size)
+        self.values = nn.Linear(emb_size, emb_size)
+        self.att_drop = nn.Dropout(dropout)
+        self.proj = nn.Linear(emb_size, emb_size)
+
+    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
+        # split keys, queries and values in num_heads
+
+        # gets queries by breaking apart each flattened patch.
+        # For the default first layer, each image patch is transformed into a new representation using a fully connected
+        # layer and then broken into 6 strips. It is not 6 horizontal strips as it is transformed under FC
+        # Based on your understanding of BP, this should be similar to the idea of group-norm in CNNs in that each head
+        # will learn a different representation of each patch, ie effectively this is num_heads FC layers in parallel
+        queries = rearrange(self.queries(x), "b n (h d) -> b h n d", h=self.num_heads)
+
+        # gets keys and values by doing a similar thing
+        keys = rearrange(self.keys(x), "b n (h d) -> b h n d", h=self.num_heads)
+        values = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
+
+        # so now we have three different representations (q, k, v) of the original image.
+
+        # Next we want to find the dot product between each query and key. We do this by rearranging (done before) to
+        # be batch x head x patches x d-dim rep. Then the dot product between every d-dim rep of every patch for each
+        # head is calculated
+
+        # idea! if my understanding of the orthogonality constraint on information flow between representations is
+        # correct we should be able to remove either queries or keys and do the dot product with itself to get similar
+        # performance while saving on 1/2 of the compute.
+
+        dotprod = torch.einsum("b h n d, b h m d -> b h n m", queries, keys)
+
+        scaling = self.emb_size ** (1 / 2)
+        att = F.softmax(dotprod, dim=-1) / scaling
+        # soft-max is exponential so dividing by the scaling will make the softmax values smaller so its easier
+
+        # Dropout layer
+        att = self.att_drop(att)
+
+        # Perform matrix multiplication to get attention weighted values
+        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.proj(out)
+
+        return out
+
+
+class BudgetAttentionTwo(nn.Module):
+    def __init__(self, emb_size: int = 512, num_heads: int = 8, dropout: float = 0):
+        # Theoretically, the larger the embedding size, the more information can be learnt
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.keys = nn.Linear(emb_size, emb_size)
+        self.values = nn.Linear(emb_size, emb_size)
+        self.att_drop = nn.Dropout(dropout)
+
+    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
+        # gets keys and values by doing a similar thing
+        keys = rearrange(self.keys(x), "b n (h d) -> b h n d", h=self.num_heads)
+        values = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
+
+        dotprod = torch.einsum("b h n d, b h m d -> b h n m", keys, keys)
+
+        scaling = self.emb_size ** (1 / 2)
+        att = F.softmax(dotprod, dim=-1) / scaling
+        # soft-max is exponential so dividing by the scaling will make the softmax values smaller so its easier
+
+        # Dropout layer
+        att = self.att_drop(att)
+
+        # Perform matrix multiplication to get attention weighted values
+        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
+        out = rearrange(out, "b h n d -> b n (h d)")
+
+        return out
+
+
+class ResidualAdd(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x += res
+        return x
+
+
+class FeedForwardBlock(nn.Sequential):
+    def __init__(self, emb_size: int, drop_p: float = 0.):
+        super().__init__(
+            nn.Linear(emb_size, emb_size),
+            nn.SiLU(),
+            nn.Dropout(drop_p),
+            nn.Linear(emb_size, emb_size)
+        )
+
+
+class BudgetFeedForwardBlock(nn.Sequential):
+    def __init__(self, emb_size: int, drop_p: float = 0.):
+        super().__init__(
+            nn.Linear(emb_size, emb_size),
+            nn.SiLU(),
+            nn.Dropout(drop_p),
+        )
+
+
+class TransformerEncoderBlock(nn.Sequential):
+    def __init__(self,
+                 emb_size: int = 768,
+                 drop_p: float = 0.,
+                 forward_drop_p: float = 0.,
+                 **kwargs):
+        super().__init__(
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MultiHeadAttention(emb_size, **kwargs),
+                nn.Dropout(drop_p)
+            )),
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                FeedForwardBlock(
+                    emb_size, drop_p=forward_drop_p),
+                nn.Dropout(drop_p)
+            )
+            ))
+
+
+class TransformerEncoder(nn.Sequential):
+    def __init__(self, depth: int = 3, **kwargs):
+        super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
+
+
+class TransformerBudgetEncoderBlockThree(nn.Sequential):
+    def __init__(self,
+                 emb_size: int = 768,
+                 drop_p: float = 0.,
+                 forward_drop_p: float = 0.,
+                 **kwargs):
+        super().__init__(
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                BudgetAttentionTwo(emb_size, **kwargs),
+                nn.Dropout(drop_p)
+            )),
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                BudgetFeedForwardBlock(
+                    emb_size, drop_p=forward_drop_p),
+                nn.Dropout(drop_p)
+            )
+            ))
+
+
+class TransformerBudgetEncoderThree(nn.Sequential):
+    def __init__(self, depth: int = 3, **kwargs):
+        super().__init__(*[TransformerBudgetEncoderBlockThree(**kwargs) for _ in range(depth)])
+
+
+class ClassificationHead(nn.Sequential):
+    def __init__(self, emb_size: int = 768, n_classes: int = 1000):
+        super().__init__(
+            Reduce('b n e -> b e', reduction='mean'),
+            nn.LayerNorm(emb_size),
+            nn.Linear(emb_size, n_classes))
+
+
+class ViT(nn.Sequential):
+    def __init__(self,
+                 in_channels: int = 3,
+                 patch_size: int = 8,
+                 emb_size: int = 128,
+                 img_size: int = 32,
+                 depth: int = 12,
+                 n_classes: int = 10,
+                 **kwargs):
+        super().__init__(
+            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
+            TransformerEncoder(depth, emb_size=emb_size, **kwargs),
+            ClassificationHead(emb_size, n_classes)
+        )
+
+class BViTThree(nn.Sequential):
+    def __init__(self,
+                 in_channels: int = 3,
+                 patch_size: int = 8,
+                 emb_size: int = 128,
+                 img_size: int = 32,
+                 depth: int = 12,
+                 n_classes: int = 10,
+                 **kwargs):
+        super().__init__(
+            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
+            TransformerBudgetEncoderThree(depth, emb_size=emb_size, **kwargs),
+            ClassificationHead(emb_size, n_classes)
+        )
---
+++ main.py
@@ -0,0 +1,147 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import gc
+import time
+import torchvision.transforms as transforms
+
+from SBF_arch import ViT, BViTThree
+
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    return correct
+
+
+def train(trainloader, testloader, model, loss_fn, optimizer, device, num):
+    size = len(trainloader.dataset)
+    model.train()
+    modelTrainHistory = []
+    modelTestHistory = []
+    timeNow = time.time_ns()
+    for batch, (X, y) in enumerate(trainloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 1000 == 0:
+            print(time.time_ns() - timeNow)
+
+            loss, current = loss.item(), batch * len(X)
+
+            modelTrainHistory.append(loss)
+
+            testLoss = test(testloader, model, loss_fn, device)
+
+            modelTestHistory.append(testLoss)
+            model.train()
+
+            print(f"Model {num} \n test loss: {testLoss:>7f} loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+            timeNow = time.time_ns()
+    return modelTrainHistory, modelTestHistory
+
+
+def run(trainset, testset, model, optimizer, device, epochs, loss_fn, iteration, num):
+    trainH, testH = [], []
+    for t in range(epochs):
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=10, num_workers=1)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=10, num_workers=1)
+        gc.collect()
+        print(f"Epoch {t + 1}\n-------------------------------")
+        trainT, testT = train(trainloader, testloader, model, loss_fn, optimizer, device, num)
+        trainH = trainH + trainT
+        testH = testH + testT
+
+    with open(f'modelVIT{num}TestHist{iteration}.txt', 'w') as fp:
+        for item in testH:
+            # write each item on a new line
+            fp.write("%s\n" % item)
+
+
+def main(num):
+    epochs = 20
+    device = "cuda"
+    print(device)
+    learningrate = 10e-5
+
+    transform = transforms.ToTensor()
+
+    trainset = datasets.CIFAR10(root='./data', train=True,
+                                download=True, transform=transform)
+    testset = datasets.CIFAR10(root='./data', train=False,
+                               download=True, transform=transform)
+
+    for i in range(5):
+        print(f"-------starting {i + 1}th iteration!-------")
+        ms512 = ViT(in_channels=3,
+                    patch_size=8,
+                    emb_size=512,
+                    img_size=32,
+                    depth=6,
+                    n_classes=10).to(device)
+        ls512 = nn.CrossEntropyLoss()
+        optimizerMs512 = torch.optim.AdamW(ms512.parameters(), lr=learningrate)
+
+        m0512 = ViT(in_channels=3,
+                    patch_size=8,
+                    emb_size=512,
+                    img_size=32,
+                    depth=12,
+                    n_classes=10).to(device)
+        l0512 = nn.CrossEntropyLoss()
+        optimizerM0512 = torch.optim.AdamW(m0512.parameters(), lr=learningrate)
+
+        #         m1 = BViTOne(in_channels = 3,
+        #                 patch_size = 8,
+        #                 emb_size = 128,
+        #                 img_size = 32,
+        #                 depth = 12,
+        #                 n_classes = 10).to(device)
+        #         l1 = nn.CrossEntropyLoss()
+        #         optimizerM1 = torch.optim.AdamW(m1.parameters(), lr=learningrate)
+
+        #         m2 = BViTTwo(in_channels = 3,
+        #                 patch_size = 8,
+        #                 emb_size = 128,
+        #                 img_size = 32,
+        #                 depth = 12,
+        #                 n_classes = 10).to(device)
+        #         l2 = nn.CrossEntropyLoss()
+        #         optimizerM2 = torch.optim.AdamW(m2.parameters(), lr=learningrate)
+
+        m3512 = BViTThree(in_channels=3,
+                          patch_size=8,
+                          emb_size=512,
+                          img_size=32,
+                          depth=12,
+                          n_classes=10).to(device)
+        l3512 = nn.CrossEntropyLoss()
+        optimizerM3512 = torch.optim.AdamW(m3512.parameters(), lr=learningrate)
+
+        print(num)
+        run(trainset, testset, locals()[f"m{num}"], locals()[f"optimizerM{num}"], device, epochs, locals()[f"l{num}"],
+            i, num)
+
+
+if __name__ == '__main__':
+    for i in ['s512']:
+        main(i)
